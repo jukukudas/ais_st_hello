44cc1bd1e29cfbea89703f91b1ccf6fd0aec88e5
jukukudas <127728603+jukukudas@users.noreply.github.com>
2023-04-26 11:17:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -34,3 +34,9 @@ filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
